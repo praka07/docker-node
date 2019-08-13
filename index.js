4b17c749f8398b1d5d6b4f839bf560670fc41204
praka07 <prakashbtw.007@gmail.com>
2019-08-13 10:25:10 +0530

    added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-# docker-node
+'use strict';
+var http = require('http'); // 1 - Import Node.js core module
+
+var server = http.createServer(function (req, res) {   
+res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('welcome to node world');
+});
+
+server.listen(4040); //3 - listen for any incoming requests
+
+console.log('Node.js web server at port 4040 is running..')
