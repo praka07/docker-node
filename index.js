dd1fe06a660bad41bf9801d52dffd0ef1cfc493c
praka07 <33973861+praka07@users.noreply.github.com>
2019-08-20 19:28:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http'); // 1 - Import Node.js core module
 
 var server = http.createServer(function (req, res) {   
 res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('welcome to node world !!!');
+  res.end('hello !! it works');
 });
 
 server.listen(4040); //3 - listen for any incoming requests
